30bf1c3247e4d77b1892b58ef61c2851217e0dc7
Ruchitn21 <nigam21ruchit@gmail.com>
2025-07-02 13:54:10 +0530

    First Commit

--- src/main/java/org/ticketBooking/service/UserBookingService.java
+++ src/main/java/org/ticketBooking/service/UserBookingService.java
@@ -3,6 +3,7 @@ package org.ticketBooking.service;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.ticketBooking.entities.User;
+import org.ticketBooking.util.UserServiceUtil;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,9 +29,12 @@ public class UserBookingService {
         userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
     }
 
-//    public boolean login() {
-//        Optional<User> foundUser = userList.stream().filter(user1-> {
-//            return user1.getName().equals(user.getName()) &&
-//        })
+    public boolean login() {
+        Optional<User> userFound = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && ;
+                   UserServiceUtil.checkPassword(user.getPassword(), user1.getPassword());
+        }).findFirst();
+
+        return userFound.isPresent();
     }
 }
